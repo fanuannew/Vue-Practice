4e9c42255d62589468fafcbe90081f8863ae4200
fanuannew <fanuannew@gmail.com>
2018-09-28 18:30:03 +0800

    last update

--- VueCLI/vuecliTest/src/components/Count.vue
+++ VueCLI/vuecliTest/src/components/Count.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h2>{{msg}}</h2><hr/>
-    <h3>{{$store.state.count}}</h3>
+    <h3>{{$store.state.count}}--{{count}}</h3>
     <p>
       <button @click="$store.commit('add')">+</button>
       <button @click="$store.commit('reduce')">-</button>
@@ -11,12 +11,33 @@
 
 <script>
 import store from "@/vuex/store";
+import { mapState, mapMutations, mapGetters } from "vuex";
 export default {
   data() {
     return {
       msg: "Hello Vuex"
     };
   },
+  // computed: { //第一種方法
+  //   count() {
+  //     return this.$store.state.count;
+  //   }
+  // },
+  //computed: mapState(["count"]), //第二種方法
+  // computed: mapState({
+  //   //第三種方法
+  //   count: function(state) {
+  //     return state.count;
+  //   }
+  // }),
+  computed: {
+    ...mapState(["count"]),
+    // count() {
+    //   return this.$store.getters.count;
+    // }
+    ...mapGetters(["count"])
+  },
+  methods: mapMutations(["add", "reduce"]),
   store
 };
 </script>
